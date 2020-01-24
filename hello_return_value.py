9b06c1bf6aacd9fa2d8ac9275990764fd43995c7
Rares <59968148+Rares-code@users.noreply.github.com>
2020-01-24 10:23:59 +0200

    Add files via upload

---
+++ hello_return_value.py
@@ -0,0 +1,3 @@
+def hello_name(name):
+    return ("Hello" " " + name)
+print(hello_name("Rares!"))
---
+++ max_function.py
@@ -0,0 +1,6 @@
+def max_function(x,y):
+    if x >=y:
+        return x
+    elif y>=x:
+        return y
+print(max_function(4,5))
